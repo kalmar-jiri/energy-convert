42b50e802093f970e6d8c2e32cb94f5894aa4381
Jiri Kalmar <kalmar.jiri@gmail.com>
2024-04-17 09:27:40 +0200

    Change font

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function convert() {
-  const input1 = document.getElementById("input1");
-  const input2 = document.getElementById("input2");
-  const input3 = document.getElementById("input3");
-  const input4 = document.getElementById("input4");
-  const input5 = document.getElementById("input5");
-  const input6 = document.getElementById("input6");
-  const input7 = document.getElementById("input7");
-  const input8 = document.getElementById("input8");
-  const input9 = document.getElementById("input9");
-  const input10 = document.getElementById("input10");
+  const input1 = document.getElementById('input1');
+  const input2 = document.getElementById('input2');
+  const input3 = document.getElementById('input3');
+  const input4 = document.getElementById('input4');
+  const input5 = document.getElementById('input5');
+  const input6 = document.getElementById('input6');
+  const input7 = document.getElementById('input7');
+  const input8 = document.getElementById('input8');
+  const input9 = document.getElementById('input9');
+  const input10 = document.getElementById('input10');
 
   const electronvolt = parseFloat(input1.value);
   const wavelength = parseFloat(input2.value);
@@ -42,6 +42,8 @@ function convert() {
     input8.value = eV_C.toFixed(7);
     input9.value = eV_Hz.toExponential(7);
     input10.value = eV_J.toExponential(7);
+
+    input1.blur();
   } else if (!isNaN(wavelength) && wavelength > 0) {
     // wavelength to others
     // const nm_eV = (planckConstant * speedOfLight) / (wavelength * electronCharge * 1e-9);
@@ -63,6 +65,8 @@ function convert() {
     input8.value = nm_C.toFixed(7);
     input9.value = nm_Hz.toExponential(7);
     input10.value = nm_J.toExponential(7);
+
+    input2.blur();
   } else if (!isNaN(hartree) && hartree > 0) {
     // Hartree to others
     const hartree_eV = hartree * 27.211386245988;
@@ -83,6 +87,8 @@ function convert() {
     input8.value = hartree_C.toFixed(7);
     input9.value = hartree_Hz.toExponential(7);
     input10.value = hartree_J.toExponential(7);
+
+    input3.blur();
   } else if (!isNaN(kcalmol) && kcalmol > 0) {
     // kcal/mol to others
     const kcalmol_eV = kcalmol * 0.04336410424180094;
@@ -103,6 +109,8 @@ function convert() {
     input8.value = kcalmol_C.toFixed(7);
     input9.value = kcalmol_Hz.toExponential(7);
     input10.value = kcalmol_J.toExponential(7);
+
+    input4.blur();
   } else if (!isNaN(kJmol) && kJmol > 0) {
     // kJ/mol to others
     const kJmol_eV = kJmol * 0.010364269656262175;
@@ -123,6 +131,8 @@ function convert() {
     input8.value = kJmol_C.toFixed(7);
     input9.value = kJmol_Hz.toExponential(7);
     input10.value = kJmol_J.toExponential(7);
+
+    input5.blur();
   } else if (!isNaN(cm) && cm > 0) {
     // cm^-1 to others
     const cm_eV = cm * 0.00012398419843320026;
@@ -143,6 +153,8 @@ function convert() {
     input8.value = cm_C.toFixed(7);
     input9.value = cm_Hz.toExponential(7);
     input10.value = cm_J.toExponential(7);
+
+    input6.blur();
   } else if (!isNaN(K) && K > 0) {
     // K to others
     const K_eV = K * 8.617333262145179e-5;
@@ -163,6 +175,8 @@ function convert() {
     input8.value = K_C.toFixed(7);
     input9.value = K_Hz.toExponential(7);
     input10.value = K_J.toExponential(7);
+
+    input7.blur();
   } else if (!isNaN(C) && C >= -273.15) {
     // C to others
     const C_K = C + 273.15;
@@ -183,6 +197,8 @@ function convert() {
     input7.value = C_K.toFixed(7);
     input9.value = C_Hz.toExponential(7);
     input10.value = C_J.toExponential(7);
+
+    input8.blur();
   } else if (!isNaN(Hz) && Hz > 0) {
     // Hz to others
     const Hz_eV = Hz * 4.135667696923859e-15;
@@ -203,6 +219,8 @@ function convert() {
     input7.value = Hz_K.toExponential(7);
     input8.value = Hz_C.toFixed(7);
     input10.value = Hz_J.toExponential(7);
+
+    input9.blur();
   } else if (!isNaN(J) && J > 0) {
     // Hz to others
     const J_eV = J / 1.602176634e-19;
@@ -223,28 +241,30 @@ function convert() {
     input7.value = J_K.toExponential(7);
     input8.value = J_C.toExponential(7);
     input9.value = J_Hz.toExponential(7);
+
+    input10.blur();
   } else {
-    alert("Please enter a valid positive number.");
+    alert('Please enter a valid positive number.');
   }
 }
 
 function clearFields() {
-  input1.value = "";
-  input2.value = "";
-  input3.value = "";
-  input4.value = "";
-  input5.value = "";
-  input6.value = "";
-  input7.value = "";
-  input8.value = "";
-  input9.value = "";
-  input10.value = "";
+  input1.value = '';
+  input2.value = '';
+  input3.value = '';
+  input4.value = '';
+  input5.value = '';
+  input6.value = '';
+  input7.value = '';
+  input8.value = '';
+  input9.value = '';
+  input10.value = '';
 }
 
 function handleKeyDown(event) {
-  if (event.key === "Enter") {
+  if (event.key === 'Enter') {
     convert();
-  } else if (event.key === "x") {
+  } else if (event.key === 'x') {
     clearFields();
   }
 }
